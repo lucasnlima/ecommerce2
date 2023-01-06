7443a72cbf67730fb4938252e096aa792f759776
Lucas Lima <lucas.lima@bwtech.com>
2023-01-05 21:06:38 -0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Changes to be committed: #	modified:   .vscode/settings.json #	modified:   pom.xml #	new file:   src/test/java/com/study/controller/ShopControllerTest.java #	modified:   src/test/java/com/study/repository/ShopRepositoryTest.java #

---
+++ src/test/java/com/study/controller/ShopControllerTest.java
@@ -0,0 +1,31 @@
+// package com.study.controller;
+
+// import io.micronaut.http.client.annotation.Client;
+
+// import static org.mockito.Mockito.mock;
+
+// import java.security.PublicKey;
+
+// import com.study.repository.ShopRepository;
+// // N'ao consegui achar essa dependencia
+// import io.micronaut.http.client.RxHttpClient;
+// import io.micronaut.test.annotation.MockBean;
+// import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+// import jakarta.inject.Inject;
+
+// @MicronautTest
+// public class ShopControllerTest {
+
+//     @MockBean(ShopRepository.class)
+//     public ShopRepository mockShopRepository(){
+//         return mock(ShopRepository.class);
+//     };
+
+//     public ShopControllerTest() {
+//     }
+    
+//     @Inject
+//     @Client("/")
+//     private RxHttpClient client;
+
+// }
